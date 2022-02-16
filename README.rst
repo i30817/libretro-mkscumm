50217974005c9bef5f21b83a2d8ecee0b210d329
i30817 <i30817@gmail.com>
2022-02-16 18:35:53 +0000

    Release 2.0.0

--- README.rst
+++ README.rst
@@ -5,7 +5,7 @@ Creating playlists for the scummvm core in retroarch is complicated.
 
 1. Add the games, by loading, then starting the core from retroarch, then
 'mass add' all the games (note this isn't the retroarch scanner - it's a
-functionality of the core GUI - under the arrow next to 'Add Game...' in the
+functionality of the core GUI - click the arrow next to 'Add Game...' in the
 classic theme - the retroarch scan directory function should not be used for
 scummvm games, and the manual scanner is unneeded with this program).
 
--- libretro_mkscumm/__main__.py
+++ libretro_mkscumm/__main__.py
@@ -76,7 +76,7 @@ def mainaux(cfg: Path = typer.Argument(CONFIG, help='Path to the retroarch cfg f
 	"""
 	Creating playlists for the scummvm core in retroarch is complicated.
 
-	1. Add the games, by loading, then starting the core from retroarch, then 'mass add' all the games (note this isn't the retroarch scanner - it's a functionality of the core GUI - under the arrow next to 'Add Game...' in the classic theme - the retroarch scan directory function should not be used for scummvm games, and the manual scanner is unneeded with this program).
+	1. Add the games, by loading, then starting the core from retroarch, then 'mass add' all the games (note this isn't the retroarch scanner - it's a functionality of the core GUI - click the arrow next to 'Add Game...' in the classic theme - the retroarch scan directory function should not be used for scummvm games, and the manual scanner is unneeded with this program).
 	
 	2. Download the required engine files and extract them to the right dir. I recommend installing the scummvm required files with 'core system files downloader' in retroarch if you're running the normal core¹.
 	
