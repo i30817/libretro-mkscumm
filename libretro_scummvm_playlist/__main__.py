42bf084d93d014bed04a3f2f0d6b84bbf071b8a7
i30817 <i30817@gmail.com>
2023-12-03 07:22:29 +0000

    Find retroarch.cfg in android

--- libretro_scummvm_playlist/__main__.py
+++ libretro_scummvm_playlist/__main__.py
@@ -49,6 +49,8 @@ if sys.platform == 'win32': #this is for 64 bits too
                 CONFIG = Path(var, 'RetroArch', 'retroarch.cfg')
 elif sys.platform == 'darwin':
     CONFIG = Path(Path.home(), 'Library', 'Application Support', 'RetroArch', 'config', 'retroarch.cfg')
+elif hasattr(sys, 'getandroidapilevel'):
+    CONFIG = Path("/", "storage", "emulated", "0", "Android", "data", "com.retroarch.aarch64", "files", "retroarch.cfg")
 else: #all the rest based on linux. If they arent based on linux, they'll try the else and fail harmlessly later
     var = os.getenv('XDG_CONFIG_HOME')
     if var:
